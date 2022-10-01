d2268e20c3f98ca6a84de0e91556c5374d6921d6
Dave Anderson <dave.anderson@dominionvoting.com>
2022-10-01 08:59:05 -0600

    Neovim: Added missing file.

---
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,30 @@
+local status_ok, treesitter = pcall(require, "nvim-treesitter.configs")
+if not status_ok then return end
+
+treesitter.setup({
+  ensure_installed = {},
+  sync_install = false,
+  ignore_install = {},
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+  context_commentstring = {
+    enable = true,
+    enable_autocmd = false,
+  },
+  rainbow = {
+    enable = true,
+    disable = { "html" },
+    extended_mode = false,
+    max_file_lines = nil,
+  },
+  autopairs = { enable = true },
+  autotag = { enable = true },
+  incremental_selection = { enable = true },
+  indent = { enable = false },
+})
+
+if vim.loop.os_uname().sysname == "Windows_NT" then
+    require 'nvim-treesitter.install'.compilers = {"cl", "gcc"}
+end
