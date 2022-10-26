6ce7a1a269bdb5e57e4a77ba4c719e57c21f046b
Dave Anderson <dave.anderson@dominionvoting.com>
2022-10-26 08:40:52 -0600

    Emacs: Switched to Doom One theme.

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -5,6 +5,8 @@
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
  '(company-dabbrev-downcase nil)
+ '(custom-safe-themes
+   '("da75eceab6bea9298e04ce5b4b07349f8c02da305734f7c0c8c6af7b5eaa9738" "02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" "60ada0ff6b91687f1a04cc17ad04119e59a7542644c7c59fc135909499400ab8" default))
  '(display-line-numbers 'relative)
  '(doom-modeline-buffer-file-name-style 'buffer-name)
  '(evil-undo-system 'undo-tree)
@@ -13,7 +15,7 @@
  '(ispell-program-name "aspell")
  '(markdown-command "pandoc -f markdown -t html")
  '(package-selected-packages
-   '(tree-sitter-langs tree-sitter-indent tree-sitter js2-mode lsp-treemacs lsp-ui lsp-mode smartparens projectile flycheck "ivy" ivy evil-collection magit which-key org ranger evil-cleverparens evil-surround lsp-python-ms dap-mode counsel-projectile dired-single all-the-icons-dired tern winum doom-modeline all-the-icons helpful ivy-rich rainbow-delimiters command-log-mode evil-org powershell evil evil-commentary cider clojure-mode diminish yasnippet-snippets yasnippet auctex general ag omnisharp company hc-zenburn-theme counsel csharp-mode snippet evil undo-tree use-package))
+   '(one-themes lsp-pyright doom-themes tree-sitter-langs tree-sitter-indent tree-sitter js2-mode lsp-treemacs lsp-ui lsp-mode smartparens projectile flycheck "ivy" ivy evil-collection magit which-key org ranger evil-cleverparens evil-surround lsp-python-ms dap-mode counsel-projectile dired-single all-the-icons-dired tern winum doom-modeline all-the-icons helpful ivy-rich rainbow-delimiters command-log-mode evil-org powershell evil evil-commentary cider clojure-mode diminish yasnippet-snippets yasnippet auctex general ag omnisharp company hc-zenburn-theme counsel csharp-mode snippet evil undo-tree use-package))
  '(split-window-preferred-function 'split-window-more-sensibly)
  '(style-cop-batch-path "c:/dvs/StyleCop")
  '(undo-tree-auto-save-history nil)
--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -34,14 +34,24 @@
 
 (use-package avy)
 
-(use-package hc-zenburn-theme
+(use-package doom-themes
   :config
-  (load-theme 'hc-zenburn t))
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-one t)
 
-(custom-theme-set-faces
- 'hc-zenburn
- '(line-number-current-line ((t (:foreground "#A0EDF0"))))
-)
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Enable custom neotree theme (all-the-icons must be installed!)
+  (doom-themes-neotree-config)
+  ;; or for treemacs users
+  (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
+  (doom-themes-treemacs-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
+
+(set-face-foreground 'line-number "#5D5D5D")
 
 (use-package all-the-icons)
 
