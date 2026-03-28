d55881bfcc3e7f9d4240189c5cf657facbe01358
Dave Anderson <dave.anderson@libertyvote.com>
2026-03-28 10:19:00 -0600

    NeoVim: Added basic VS Code support.

--- neovim/.config/nvim/lua/lazy/plugins.lua
+++ neovim/.config/nvim/lua/lazy/plugins.lua
@@ -1,29 +1,33 @@
-require('lazy').setup({
-
-  -- require 'plugins/gitsigns',
-  require 'plugins/which-key',
+local plugins = {
+  -- Shared plugins (work in both VS Code and Neovim)
   require 'plugins/comment',
   require 'plugins/hop',
-  require 'plugins/tree',
-  require 'plugins/telescope',
-  require 'plugins/lspconfig',
-  require 'plugins/winpick',
-  require 'plugins/bufdelete',
-  require 'plugins/bufexplorer',
-  require 'plugins/completion',
-  -- require 'plugins/tokyonight',
-  require 'plugins/onedarkpro',
-  require 'plugins/lualine',
-  require 'plugins/vim-fugitive',
-  require 'plugins/vim-gitgutter',
-  require 'plugins/indent-blankline',
   require 'plugins/surround',
-  require 'plugins/aerial',
-  require 'plugins/autopairs',
-  require 'plugins/terminal',
-  require 'plugins/treesitter',
-}, {
-  ui = {
+  require 'plugins/bufdelete',
+}
+
+if not vim.g.vscode then
+  vim.list_extend(plugins, {
+    require 'plugins/which-key',
+    require 'plugins/tree',
+    require 'plugins/telescope',
+    require 'plugins/lspconfig',
+    require 'plugins/winpick',
+    require 'plugins/bufexplorer',
+    require 'plugins/completion',
+    require 'plugins/onedarkpro',
+    require 'plugins/lualine',
+    require 'plugins/vim-fugitive',
+    require 'plugins/vim-gitgutter',
+    require 'plugins/indent-blankline',
+    require 'plugins/aerial',
+    require 'plugins/autopairs',
+    require 'plugins/terminal',
+    require 'plugins/treesitter',
+  })
+end
+
+require('lazy').setup(plugins, {
     icons = {
       cmd = '⌘',
       config = '🛠',
@@ -38,8 +42,7 @@ require('lazy').setup({
       start = '🚀',
       task = '📌',
       lazy = '💤 ',
-    },
-  },
+      }
 })
 
 -- vim: ts=2 sts=2 sw=2 et
